abc85b6c64f0d3b56887a809234a52690b4cc5b9
Nohaz Hazanoir <nohaz.hazanoir@gmail.com>
2021-03-09 14:49:45 +0800

    Add SELFCERT

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+app = express()
 app.set("/", "html");
 app.use(express.static(path.join(__dirname, "/")));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.render('index');
+   res.render('index');
 });
 
-app.listen(PORT, () =>
-  console.log(""));
+var server = https.createServer(options, app);
+
+server.listen(PORT, () => {
+  console.log("server starting on port : " + PORT)
+});
