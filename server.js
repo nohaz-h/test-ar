cd1ebb2d19774fe5a8ee8dafd515b351204c8154
Nohaz Hazanoir <nohaz.hazanoir@gmail.com>
2021-03-09 13:59:49 +0800

    Working Sadiq code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "/")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () =>
+  console.log(""));
