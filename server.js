aff5bef98890f5b310bbf30ce7682424c8802b00
Nohaz Hazanoir <nohaz.hazanoir@gmail.com>
2021-03-09 14:52:38 +0800

    Revert "Add SELFCERT"
    This reverts commit abc85b6c64f0d3b56887a809234a52690b4cc5b9.

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
-
-app = express()
 app.set("/", "html");
 app.use(express.static(path.join(__dirname, "/")));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-   res.render('index');
+  res.render('index');
 });
 
-var server = https.createServer(options, app);
-
-server.listen(PORT, () => {
-  console.log("server starting on port : " + PORT)
-});
+app.listen(PORT, () =>
+  console.log(""));
